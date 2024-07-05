25f47fe0417a3364e42dc0d1b3fe4d83b8932ca6
jetlai <jetlai2003@gmail.com>
2024-07-05 16:40:05 +0800

    Added log in

--- Guest/login.aspx.cs
+++ Guest/login.aspx.cs
@@ -5,18 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WAPP_Assignment
+namespace WAPP_Assignment.Guest
 {
-    public partial class template : System.Web.UI.MasterPage
+    public partial class login : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
-
-        protected void LinkButton1_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("login.aspx");
         }
     }
 }
