c6c7d321a82b887e9d0ce935ec3b2676fdff98b4
jetlai <jetlai2003@gmail.com>
2024-07-07 16:09:46 +0800

    Copy n paste new template.master and css to Guest folder. Also create login.aspx with css.

--- Guest/login.aspx
+++ Guest/login.aspx
@@ -1,5 +1,31 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/template.Master" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="WAPP_Assignment.Guest.login" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Guest/template.Master" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="WAPP_Assignment.Guest.login" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="login.css" rel="stylesheet" />
 </asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="login-container"> 
+        <h2 class="login-title">User Login</h2>
+
+        <div class="form-group">
+            <asp:Label ID="Label2" runat="server" Text="Username:"></asp:Label>
+            <asp:TextBox ID="username" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+
+        <div class="form-group">
+            <asp:Label ID="Label3" runat="server" Text="Password:"></asp:Label>
+            <asp:TextBox ID="pwd" runat="server" CssClass="form-control" TextMode="Password"></asp:TextBox>
+        </div>
+
+        <asp:Label ID="errorMsg" runat="server" CssClass="error-message" Text=""></asp:Label>
+        <br />
+
+        <asp:Button ID="Button1" runat="server" Text="Login" CssClass="login-button" />
+        <br />
+
+        <div class="signup-link">
+            <asp:Label ID="Label4" runat="server" Text="Do not have an account?"></asp:Label>
+            <asp:LinkButton ID="LinkButton2" runat="server" CssClass="signup-button">Sign Up</asp:LinkButton>
+        </div>
+    </div>
 </asp:Content>
--- Guest/login.aspx.cs
+++ Guest/login.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -13,5 +15,48 @@ namespace WAPP_Assignment.Guest
         {
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            {
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("Select count(*) from userTable where username = '" +
+                    username.Text + "' and Password = '" + pwd.Text + " ' ", con);
+                int count = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+
+                if (count > 0)
+                {
+                    SqlCommand cmdType = new SqlCommand("select fname, usertype from userTable where username = '" + username.Text + " ' ", con);
+
+                    SqlDataReader dr = cmdType.ExecuteReader();
+
+                    String type = " ";
+                    String name = " ";
+
+                    while (dr.Read())
+                    {
+                        type = dr["usertype"].ToString().Trim();
+                        name = dr["fname"].ToString().Trim();
+                    }
+                    Session["userName"] = username.Text;
+
+                    if (type == "admin")
+                        Response.Redirect("adminDashboard.aspx");
+                    else if (type == "member")
+                        Response.Redirect("memberDashboard");
+                }
+                else
+                {
+                    errorMsg.Visible = true;
+                    errorMsg.ForeColor = System.Drawing.Color.Red;
+                    errorMsg.Text = "Username and Password mismatch!";
+                    return;
+                }
+                con.Close();
+            }
+        }
     }
+
 }
