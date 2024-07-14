0ac6772bb3b3a42d2a0b09e4775984f27ae93003
jetlai <jetlai2003@gmail.com>
2024-07-14 16:42:17 +0800

    edit profile aspx and css

---
+++ Guest/editProfile.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WAPP_Assignment.Guest
+{
+    public partial class editProfile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonSaveProfile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
