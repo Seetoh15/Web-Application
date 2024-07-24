5724013b5a2c742246e88879966f8f481ac22b45
kair072903 <wongjinkaijoshua@gmail.com>
2024-07-24 21:54:24 +0800

    change files

--- Member/englishQuiz.aspx.cs
+++ Member/englishQuiz.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,11 @@ namespace WAPP_Assignment.Member
 {
     public partial class englishQuiz : System.Web.UI.Page
     {
+        private string connectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+           
         }
     }
-}
+    }
